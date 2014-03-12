2b29394af2c46d0c113308f3d54d38a18ab34872
Tery Lim <kumabotz@gmail.com>
2014-03-12 09:33:48 +0800

    Add employee collection

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
+var express = require('express'),
+    routes = require('./routes'),
+    http = require('http'),
+    path = require('path'),
+    EmployeeProvider = require('./employeeprovider').EmployeeProvider;
 
 var app = express();
 
@@ -17,8 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
@@ -29,8 +28,31 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+var employeeProvider = new EmployeeProvider('localhost', 27017);
+
+app.get('/', function(req, res) {
+  employeeProvider.findAll(function(error, emps) {
+    res.render('index', {
+      title: 'Employees',
+      employees: emps
+    });
+  });
+});
+
+app.get('/employee/new', function(req, res) {
+  res.render('employee_new', {
+    title: 'New Employee'
+  });
+});
+
+app.post('/employee/new', function(req, res) {
+  employeeProvider.save({
+    title: req.param('title'),
+    name: req.param('name')
+  }, function(error, docs) {
+    res.redirect('/');
+  });
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
